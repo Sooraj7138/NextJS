6a8da5ff7de9c602a6a36f31b550b4532855620d
Sooraj7138 <soorajsree7138@gmail.com>
2026-01-14 17:12:36 +0530

    initial commit

---
+++ models/postModels.js
@@ -0,0 +1,15 @@
+import { Schema, model, models } from "mongoose";
+
+const postSchema = new Schema({
+    title: String,
+    description: String,
+    image: String
+}, {toJSON: {virtuals: true}});
+
+postSchema.virtual('short_description').get(function(){
+    return this.description.substr(0,150)+"....."
+})
+
+const PostModel = models.Post || model('Post', postSchema)
+
+export default PostModel;
